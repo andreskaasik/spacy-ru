d4164d2c8d4b880d7816d7ab0148c9d965d59998
Andres Kaasik <andreskaasik@gmail.com>
2019-07-08 17:01:24 +0300

    Removed "ner" folder from package data list.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
    name='spacy-ru',
    version='0.1.0',
    packages=['ru2'],
-   package_data={'ru2': ['ner/*', 'parser/*', 'tagger/*', 'vocab/*', 'meta.json', 'tokenizer']},
+   package_data={'ru2': ['parser/*', 'tagger/*', 'vocab/*', 'meta.json', 'tokenizer']},
    author='Yuri Baburov',
    author_email='burchik@gmail.com',
    install_requires=['spacy>=2.0', 'pymorphy2>=0.8']
